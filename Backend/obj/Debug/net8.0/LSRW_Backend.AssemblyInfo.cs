e7a8bad0b12e473634d59aa4b189e56d6aad95b5
Mudit Bhatt ♐ <154462822+muditbhatt-5@users.noreply.github.com>
2026-08-31 21:28:30 +0530

    Mudit: Build changes.

--- Backend/obj/Debug/net8.0/LSRW_Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/LSRW_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LSRW_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67b321ea02775b533c1d926cb8dfb1e0ad0549f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6502de24b38d814fb9307dda403f843a61f87648")]
 [assembly: System.Reflection.AssemblyProductAttribute("LSRW_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LSRW_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
